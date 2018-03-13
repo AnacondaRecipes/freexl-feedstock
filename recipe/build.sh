4e50a2769ef978dfeb0a4feb3133629e59bb88d8
Nehal J Wani <nehaljw.kkd1@gmail.com>
2018-03-13 12:58:57 -0500

    Bump version to 1.0.5

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,7 @@
 export CFLAGS="$CFLAGS -I$PREFIX/include -L$PREFIX/lib"
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib -Wl,-rpath,${PREFIX}/lib"
 
-./configure --prefix=$PREFIX --host=$HOST
+./configure --prefix=$PREFIX --host=$HOST --build=$BUILD
 
 make
 make install
