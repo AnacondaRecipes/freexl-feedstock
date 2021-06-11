fd5b48ce71279bce8ad3881a3898f0dd4aea4954
Kai Tietz <ktietz@anaconda.com>
2021-06-11 09:10:45 +0200

    adjust for linux-aarch64

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,11 @@
 export CFLAGS="$CFLAGS -I$PREFIX/include -L$PREFIX/lib"
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib -Wl,-rpath,${PREFIX}/lib"
 
-./configure --prefix=$PREFIX --host=$HOST --build=$BUILD
+if [[ $(uname -m) = "aarch64" ]]; then
+  ./configure --prefix=$PREFIX --host=aarch64-linux-gnu --build=aarch64-linux-gnu
+else
+  ./configure --prefix=$PREFIX --host=$HOST --build=$BUILD
+fi
 
 make
 make install
