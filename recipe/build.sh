118fbaba5c1e8a2ccf806c37c0aca7d69dc38672
Kai Tietz <ktietz@anaconda.com>
2021-08-20 17:10:19 +0200

    fix license family, add support for osx-arm64

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Get an updated config.sub and config.guess
-cp $BUILD_PREFIX/share/gnuconfig/config.* .
+cp -r ${BUILD_PREFIX}/share/libtool/build-aux/config.* .
 
 export CFLAGS="$CFLAGS -I$PREFIX/include -L$PREFIX/lib"
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib -Wl,-rpath,${PREFIX}/lib"
