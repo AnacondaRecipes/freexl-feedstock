cf67f9d3b3a53ddab2eb570fec1126300f249086
Eric Lundby <Eric.Lundby@gmail.com>
2026-09-11 12:52:20 -0600

    PKG-16685: fix pre-existing build rot - pre-patched win makefiles (conda-build patch line endings)

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,11 @@
+@echo on
+
+:: makefile.vc / nmake.opt are pre-patched copies (the original source.patches
+:: fail to apply on Windows due to a conda-build patch line-ending issue).
+copy /y "%RECIPE_DIR%\win\makefile.vc" makefile.vc
+if errorlevel 1 exit 1
+copy /y "%RECIPE_DIR%\win\nmake.opt" nmake.opt
+if errorlevel 1 exit 1
 
 nmake /f makefile.vc
 if errorlevel 1 exit 1
